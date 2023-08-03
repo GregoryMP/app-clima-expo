626864981a987abd399d62bb18e36dbc703b4c3d
GregoryMP <139811595+GregoryMP@users.noreply.github.com>
2023-08-02 19:35:16 -0500

    Update App.js

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ export default function App() {
   useEffect(() => {
     const consultarClima = async () => {
       if (consultar) {
-        const appID = process.env.API_KEY;
+        const appID = API_KEY;
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
         try {
           const respuesta = await fetch(url);
